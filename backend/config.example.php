3b8577a40face1bf5921232f0670503ba426de40
Abdiqani <abqani@hotmail.com>
2026-03-28 22:44:24 +0100

    Remove old config.example and clean setup

--- backend/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('OPENAI_API_KEY', getenv('OPENAI_API_KEY'));
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
-?>
