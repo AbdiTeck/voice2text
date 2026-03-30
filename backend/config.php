faef57d5748aecf6ec5b4b540f83f1a31b2f7032
Abdiqani <abqani@hotmail.com>
2026-03-30 02:10:16 +0200

    Add safe config.php using env vars

---
+++ backend/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("OPENAI_API_KEY", getenv("OPENAI_API_KEY"));
+
+define("DB_HOST", getenv("DB_HOST"));
+define("DB_PORT", getenv("DB_PORT"));
+define("DB_USER", getenv("DB_USER"));
+define("DB_PASS", getenv("DB_PASS"));
+define("DB_NAME", getenv("DB_NAME"));
